327fbf0b3ed7fc77fa48acf3327542202bee3219
miguel loaiza <luismi02794@gmail.com>
2025-02-18 21:55:39 -0500

    Jquerry

--- 09-jQuery/js/app.js
+++ 09-jQuery/js/app.js
@@ -1,19 +1,50 @@
-$(function(){
-    $('footer').on('click', '#add', function() {
-        if($('#input-task').val().length > 0) {
-            
+$(function () {
+    //count task//
+    countTasks()
+    countRemains()
+    //add task//
+    $('footer').on('click', '#add', function () {
+        if ($('#input-task').val().length > 0) {
+
             $task = '<article> \
-                <input type="checkbox" checked> \
-                <p>'+$('#input-task').val()+'</p> \
+                <input type="checkbox" > \
+                <p>'+ $('#input-task').val() + '</p> \
                 <button>&times;</button> \
             </article>'
             $('section.list').append($task)
             $('input-task').val('')
+            countTasks()
+            countRemains()
 
 
         } else {
-            alert ('Please Enter a Task')
+            alert('Please Enter a Task')
         }
 
     })
+    //Toggle Task(Remain/Done)
+    $('body').on('click', 'input[type=checkbox]', function () {
+        //if checked
+        if ($(this).prop('checked')) {
+            $(this).parent().addClass('checked')
+        } else {
+            $(this).parent().removeClass('checked')
+        }
+        countRemains()
+    })
+    //remove task
+    $('body').on('click', 'article button', function () {
+        $(this).closest('article').remove()
+    })
 })
+
+function countTasks() {
+    $('.num-task').text($('article').length)
+    $('.title-task').text($('article').length > 1 ? 'tasks' : 'task')
+}
+//count remains
+function countRemains() {
+    $remain = Math.abs($('.checked').length - $('article').length)
+    $('.num-remains').text($remain)
+    $('.title-remains').text($remain > 1 ? 'Remains' : 'Remain')
+}
